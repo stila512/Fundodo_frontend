567dc463c50d3a4612d1483443735f78df8263d9
RediChen <a0929052958@gmail.com>
2024-07-24 11:49:50 +0800

    更換logo 路徑與新增連結 icon

--- components/common/navHeader.js
+++ components/common/navHeader.js
@@ -1,13 +1,14 @@
 import Image from 'next/image';
 import scss from './navHeader.module.scss';
-import logo from '@/assets/logo_temp.png';
+import logo from '@/public/logo_temp.png';
+import { IoIosArrowDown } from 'react-icons/io';
 
 export default function NavHeader() {
   return (
     <header className={scss.layout}>
       <div className={scss.container}>
         <div className={[scss.logo, scss.imgWrap, scss.txPrimary].join(' ')}>
-          <Image src={logo} alt="FUNDODO logo" width={210} height={67} />
+          <Image src={logo} alt="FUNDODO logo" width={210} />
         </div>
         <nav>
           <ul>
@@ -16,9 +17,11 @@ export default function NavHeader() {
             </li>
             <li>
               <a href="#">商品列表</a>
+              <IoIosArrowDown />
             </li>
             <li>
               <a href="#">線上課程</a>
+              <IoIosArrowDown />
             </li>
             <li>
               <a href="#">寵物旅館</a>
