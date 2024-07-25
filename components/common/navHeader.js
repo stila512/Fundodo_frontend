d84d597c64074852c813321ac01bd6814afbebc5
RediChen <a0929052958@gmail.com>
2024-07-25 15:48:21 +0800

    更新 navHeader 新增 hover 觸發顯示的內容

--- components/common/navHeader.js
+++ components/common/navHeader.js
@@ -1,8 +1,15 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import scss from './navHeader.module.scss';
-import logo from '@/public/logo_temp.png';
-import { IoIosArrowDown } from 'react-icons/io';
+import logo from '@/public/logo.png';
+import { IoIosArrowDown, IoMdPerson } from 'react-icons/io';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { IoCart } from 'react-icons/io5';
 
+/**
+ * 導覽列元件 | Fundodo 頁面基本架構
+ * @description 高度固定為 70px
+ */
 export default function NavHeader() {
   return (
     <header className={scss.layout}>
@@ -11,23 +18,74 @@ export default function NavHeader() {
           <Image src={logo} alt="FUNDODO logo" />
         </div>
         <nav className={scss.txPrimary}>
-          <ul>
+          <ul className={scss.ulLink}>
             <li>
-              <a href="#">首頁</a>
+              <Link href="#">首頁</Link>
             </li>
-            <li>
-              <a href="#">商品列表</a>
+            <li className={scss.listBtn}>
+              <Link href="#">商品列表</Link>
               <IoIosArrowDown />
+              <ul className={scss.prodList}>
+                <li>
+                  <Link href="#">狗勾飼料</Link>
+                </li>
+                <li>
+                  <Link href="#">狗勾罐頭</Link>
+                </li>
+                <li>
+                  <Link href="#">狗勾零食</Link>
+                </li>
+                <li>
+                  <Link href="#">保健食品</Link>
+                </li>
+                <li>
+                  <Link href="#">狗勾玩具</Link>
+                </li>
+                <li>
+                  <Link href="#">外出用品</Link>
+                </li>
+              </ul>
             </li>
-            <li>
-              <a href="#">線上課程</a>
+            <li className={scss.listBtn}>
+              <Link href="#">線上課程</Link>
               <IoIosArrowDown />
+              <ul className={scss.prodList}>
+                <li>
+                  <Link href="#">課程列表</Link>
+                </li>
+                <li>
+                  <Link href="#">課程分類</Link>
+                </li>
+                <li>
+                  <Link href="#">熱門課程</Link>
+                </li>
+                <li>
+                  <Link href="#">常見問題</Link>
+                </li>
+              </ul>
             </li>
             <li>
-              <a href="#">寵物旅館</a>
+              <Link href="#">寵物旅館</Link>
             </li>
             <li>
-              <a href="#">討論區</a>
+              <Link href="#">討論區</Link>
+            </li>
+          </ul>
+          <ul className={scss.ulFunc}>
+            <li>
+              <button>
+                <AiOutlineSearch />
+              </button>
+            </li>
+            <li>
+              <a href="#">
+                <IoMdPerson />
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <IoCart />
+              </a>
             </li>
           </ul>
         </nav>
