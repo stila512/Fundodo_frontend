9a7477237af714eb976369dedeac0eae0eb17de2
RediChen <a0929052958@gmail.com>
2024-07-30 10:04:28 +0800

    取消 layout 自帶 main 標籤的規格

--- components/layout/buy/index.js
+++ components/layout/buy/index.js
@@ -4,13 +4,12 @@ import Footer from './footer';
 /**
  * Fundodo 頁面基本架構 | 購物車版本
  * @description 請以此元件包住頁面內容
- * @warning 此元件已自帶 &lt;main&gt;，請勿重複使用
  */
 export default function BuyLayout({ children }) {
   return (
     <>
       <NavHeader />
-      <main style={{ marginTop: '70px' }}>{children}</main>
+      <div style={{ marginTop: '70px' }}>{children}</div>
       <Footer />
     </>
   );
