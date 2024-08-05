eb25420464377c08aeb88f347454a812247af7bf
RediChen <a0929052958@gmail.com>
2024-08-05 22:12:46 +0800

    建立購物車新元件空檔

---
+++ pages/buy/cart/HotelCartTable.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function HotelCartTable() {
+  return (
+    <div>HotelCartTable</div>
+  )
+}
---
+++ pages/buy/cart/ProdCartTable.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ProdCartTable() {
+  return (
+    <div>ProdCartTable</div>
+  )
+}
