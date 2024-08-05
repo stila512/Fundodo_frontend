d275d6e3d4bcde4f285a4720972f87444c0e70b8
RediChen <a0929052958@gmail.com>
2024-08-05 16:36:44 +0800

    更新購物車，數量面板

--- pages/buy/cart/cartPage.js
+++ pages/buy/cart/cartPage.js
@@ -12,6 +12,37 @@ import { TbTrashX } from "react-icons/tb";
 import { RxCross2 } from "react-icons/rx";
 import { LuPlus, LuMinus } from "react-icons/lu";
 
+//TODO: Remove
+const testData = [
+  {
+    name: 'Plus+機能保健系列 泌尿道紓壓保健',
+    pic_path: 'PR0000001611.jpg',
+    sort_name: '',
+    spec_name: '1.5g x 30入',
+    price: 450,
+    qty: 1,
+    key: 'dfjiaoejga'
+  },
+  {
+    name: '花花拾便器套裝',
+    pic_path: 'PR0000002581.jpg',
+    sort_name: '補充替換袋',
+    spec_name: '單入 (拾便器+拾便袋1捲)',
+    price: 150,
+    qty: 2,
+    key: 'ef53j'
+  },
+  {
+    name: 'LINSLINS寵物漏食玩具 - 魔法小兔帽',
+    pic_path: 'PR0000003971.jpg',
+    sort_name: '魔法小兔帽',
+    spec_name: '',
+    price: 250,
+    qty: 1,
+    key: 'ld073f3j'
+  },
+];
+
 //======= API ==================================
 //== parameters
 const USER_ID = 58;//todo: test
@@ -32,16 +63,15 @@ const apiLink = `${apiBaseUrl}/carts/${USER_ID}`;
 // }
 //======= API END ==================================
 
-const NumberPanel = ({qty = 1}) => {
-  const [number, setNumber] = useState(qty);
+const NumberPanel = ({ quantity = 1, setFunc = () => { }, index = -1 }) => {
 
   return (
     <div className='hstack mx-auto p-2' style={{ width: '8rem', height: '3rem', border: '1px solid #787473' }}>
-      <div className='fx-center' onClick={() => setNumber(number - 1)}>
+      <div className='fx-center' onClick={() => setFunc(arr => arr.map((d, j) => (j === index) ? d - 1 : d))}>
         <LuMinus />
       </div>
-      <div className='flex-grow-1'>{number}</div>
-      <div className='fx-center' onClick={() => setNumber(number + 1)}>
+      <div className='flex-grow-1'>{quantity}</div>
+      <div className='fx-center' onClick={() => setFunc(arr => arr.map((d, j) => (j === index) ? d + 1 : d))}>
         <LuPlus />
       </div>
     </div>
@@ -51,18 +81,25 @@ const NumberPanel = ({qty = 1}) => {
 export default function CartPage() {
   const [dataArr, setDataArr] = useState([]);
   const [qtyArr, setQtyArr] = useState([]);
+  const [totArr, setTotArr] = useState([]);
   useEffect(() => {
-    const getData = async () => {
-      const results = await axios.get(apiLink);
+    // const getData = async () => {
+    //   const results = await axios.get(apiLink);
 
-      if (results.data.status !== 'success') {
-        throw new Error(`伺服器回應：${response.data.message}`);
-      }
-      setDataArr(results.data.result);
-    }
-    getData();
-    setQtyArr(dataArr.flatMap(d => d.qty));
+    //   if (results.data.status !== 'success') {
+    //     throw new Error(`伺服器回應：${response.data.message}`);
+    //   }
+    //   setDataArr(results.data.result);
+    // }
+    // getData();
+    // setQtyArr(dataArr.flatMap(d => d.qty));
+    setDataArr(testData);
+    setQtyArr(testData.flatMap(d => d.qty));
   }, []);
+  useEffect(() => {
+    const totList = qtyArr.map((q, i) => q * dataArr[i].price);
+    setTotArr(totList);
+  }, [qtyArr])
 
 
   const noData = (!dataArr || dataArr.length === 0);
@@ -79,64 +116,45 @@ export default function CartPage() {
           <thead>
             <tr>
               <th><TbTrashX /></th>
-              <th></th>
+              <th style={{ width: '200px' }}></th>
               <th>商品資訊</th>
               <th>規格</th>
-              <th>單價</th>
+              <th style={{ width: '9rem' }}>單價</th>
               <th>數量</th>
-              <th>小計</th>
+              <th style={{ width: '9rem' }}>小計</th>
             </tr>
           </thead>
-          <tbody>
+          <tbody className='tx-body'>
             {noData ? <></> :
-              dataArr.map((item, i_data) => {
-                let specIndicator =
-                  (item.sort_name ? 1 : 0) + (item.spec_name ? 2 : 0);
-                let infoStr = '';
-                switch (specIndicator) {
-                  case 3:
-                    infoStr = [item.sort_name, item.spec_name].join('、');
-                    break;
-                  case 2:
-                    infoStr = item.spec_name;
-                    break;
-                  case 1:
-                    infoStr = item.sort_name;
-                    break;
-                  case 0:
-                  default:
-                    break;
-                }
-                const imgPath = '/pic-prod/' + item.pic_path;
-                return (
-                  <tr key={item.key}>
-                    <td>
-                      <FddBtn color='tint4' size='sm' icon callback={() => { }}>
-                        <RxCross2 />
-                      </FddBtn>
-                    </td>
-                    <td>
-                      <div className="mx-auto">
-                        <Image
-                          src={imgPath}
-                          width={100}
-                          height={100}
-                          alt={item.name}
-                        />
-                      </div>
-                    </td>
-                    <td>{item.name}</td>
-                    <td>{infoStr}</td>
-                    <td style={{ minWidth: '4rem' }}>
-                      <div className="mx-auto pe-1 tx-right" style={{ width: '3rem' }}>
-                        ${item.price}
-                      </div>
-                    </td>
-                    <td><NumberPanel qty={qtyArr[i_data]} setFunc={setQtyArr} index={i_data} /></td>
-                    <td>小計</td>
-                  </tr>
-                );
-              })
+              dataArr.map((item, i_data) => (
+                <tr key={item.key}>
+                  <td>
+                    <FddBtn color='tint4' size='sm' icon callback={() => { }}>
+                      <RxCross2 />
+                    </FddBtn>
+                  </td>
+                  <td>
+                    <Image
+                      src={'/pic-prod/' + item.pic_path}
+                      width={192}
+                      height={192}
+                      alt={item.name}
+                    />
+                  </td>
+                  <td>{item.name}</td>
+                  <td>
+                    {item.sort_name ? <p>{item.sort_name}</p> : <></>}
+                    {item.spec_name ? <p>{item.spec_name}</p> : <></>}
+                  </td>
+                  <td>
+                    <div className="mx-auto pe-1 tx-right w-fit">
+                      ${item.price}
+                    </div>
+                  </td>
+                  <td><NumberPanel quantity={qtyArr[i_data]} setFunc={setQtyArr} index={i_data} /></td>
+                  <td>${totArr[i_data]}</td>
+                </tr>
+              ))
             }
           </tbody>
         </table>
