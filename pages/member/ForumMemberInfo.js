0fea7b7cc89f9761866a82144d48dea3fb375e39
rou881212 <hongrongchen881212@gmail.com>
2024-08-06 22:34:56 +0800

    新增討論區會員頁面 部分rwd 修正

--- pages/member/ForumMemberInfo.js
+++ pages/member/ForumMemberInfo.js
@@ -14,13 +14,13 @@ import SideText from '@/components/member/SideText';
 export default function PeopleInfo() {
   return (
     <>
-      <main className={scss.PeopleInfoContainer}>
+      <main className={scss.ForumMemberInfo}>
         <div className="col-4"></div>
         <div className={`${scss.midarea} col-5`}>
-          <div className={`${scss.midtext}`}>
-          <div className={`${scss.area2} `}>Email <p>example@gmail.com <span>沒收到驗證信?</span></p></div>
-            <div className={scss.area3}>姓名 <input placeholder="姓名"></input></div>
-            <div className={scss.area4}>性別
+          <div className={scss.midtext}>
+            <div className={scss.area1}>頭像上傳 <div className={scss.avatarPic}><Image className="imgWrap" src={avatarPic} alt="Image" /></div></div>
+            <div className={scss.area2}>暱稱 <input placeholder="討論區暱稱"></input></div>
+            <div className={scss.area3}>性別
               <div className={scss.genderRadio}>
                 <input type="radio" name="gender" value="male"></input>
                 <label for="male"><Image className="imgWrap" src={radio} alt="Image" />先生</label>
@@ -28,25 +28,25 @@ export default function PeopleInfo() {
                 <label for="male"><Image className="imgWrap" src={radio} alt="Image" />女士</label>
               </div>
             </div>
-            <div className={scss.area5}>生日
+            <div className={scss.area4}>生日
               <div><label for="birthday"></label>
                 <input type="month" id="birthday" name="birthday" min="1900-01" max="2023-12" required></input>
               </div>
             </div>
-            <div className={scss.area6}>行動電話 <input placeholder="Please enter"></input></div>
-            <div className={scss.area7}>聯絡地址
-              <div className={scss.address}>
-                <div className={scss.addressTOP}>
-                  <select>
+            <div className={scss.area5}>
+              所在地
+                <div className={`${scss.area5-2} px-2`}>
+                  <select className={`mx-2`}>
                     <option>城市</option>
                   </select>
                   <select>
                     <option>地區</option>
                   </select>
                 </div>
-                <input placeholder="詳細地址"></input>
-              </div>
-
+            </div>
+            <div className={scss.area6}>有效文章 <p>3篇</p></div>
+            <div className={scss.area7}>自我介紹
+                <textarea placeholder="如活潑、安靜、友善等..."></textarea>
             </div>
             <div className={`${scss.botarea} my-5 mx-5`}>
               <button className={scss.btn1}>編輯資料</button>
@@ -54,7 +54,7 @@ export default function PeopleInfo() {
             </div>
           </div>
         </div>
-        <div className="col-5 my-5">
+        <div className="col-5">
         <SideText></SideText>
         </div>
       </main>
