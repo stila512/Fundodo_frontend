2690bd671065d39cab42f66a324aa41abb1b6776
shiba10222 <thilder10222@gmail.com>
2024-08-12 00:44:57 +0800

    prod

--- pages/prod/list/favoriteIcon.js
+++ pages/prod/list/favoriteIcon.js
@@ -3,13 +3,13 @@ import heart from '/public/prodPic/iconamoon_heart.png'
 import scss from './favoriteIcon.module.scss'
 
 export default function FavoriteIcon() {
-    return (
-        <>
-            <div className={scss.heartIconBox}>
-                <div className={scss.heartIcon}>
-                    <Image src={heart} />
-                </div>
-            </div>
-        </>
-    )
+	return (
+		<>
+			<div className={scss.heartIconBox}>
+				<div className={scss.heartIcon}>
+					<Image src={heart} />
+				</div>
+			</div>
+		</>
+	)
 }
