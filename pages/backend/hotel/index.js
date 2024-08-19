77fa8bdbba94afefbd583d6f5b83951bb62cd2c7
ry <rouying0126@gmail.com>
2024-08-19 15:19:01 +0800

    微調後台管理layout

---
+++ pages/backend/hotel/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Head from 'next/head'
+import BackendLayout from '@/components/layout/backend'
+import scss from './index.module.scss';
+
+export default function index() {
+  return (
+  <>
+     <BackendLayout>
+
+
+     </BackendLayout>
+  </>
+  )
+}
