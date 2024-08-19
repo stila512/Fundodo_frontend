c83e8c2185f9155796ce52a6946bad8b0e0949f0
RediChen <a0929052958@gmail.com>
2024-08-19 16:35:16 +0800

    新增刪除功能 但尚未連動畫面重新渲染

---
+++ pages/buy/cart/doSoftDelete.js
@@ -0,0 +1,19 @@
+import { apiBaseUrl } from '@/configs';
+import axios from 'axios';
+
+export default async function (cartID) {
+  await axios.patch(`${apiBaseUrl}/cart/${cartID}`)
+    .then(res => res)
+    .catch(err => {
+      if (err.response) {
+        //status != 2XX
+        console.error(err.response.data.message);
+      } else if (err.request) {
+        // 伺服器沒有回應
+        console.log("伺服器沒有回應，請檢查伺服器狀態");
+      } else {
+        console.log("未知的錯誤情形");
+        console.log(err);
+      }
+    });
+}
