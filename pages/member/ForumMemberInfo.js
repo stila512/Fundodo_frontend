e4eacb84af211ec79235d46b123bdd692415c37f
Rou <hongrongchen881212@gmail.com>
2024-08-19 16:44:22 +0800

    上傳圖片施工

--- pages/member/ForumMemberInfo.js
+++ pages/member/ForumMemberInfo.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useContext } from 'react';
+import { useRef, useState, useEffect, useContext } from 'react';
 import DefaultLayout from '@/components/layout/default';
 import scss from './info.module.scss';
 import Image from 'next/image';
@@ -11,21 +11,17 @@ import useAuthRedirect from '@/hooks/useAuthRedirect';
 
 export default function PeopleInfo() {
   useAuthRedirect();
+  const fileInputRef = useRef(null);
+  const [avatar, setAvatar] = useState(null);
   const { user: authUser, loading: authLoading } = useContext(AuthContext);
   const [user, setUser] = useState({
-    name: '',
-    gender: '',
-    dob: '',
-    tel: '',
-    address: ''
+    nickname: '',
+    introduce: '',
+    avatar_file: ''
   })
 
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-
-  // 新增狀態來存儲選擇的城市和區域
-  const [selectedCity, setSelectedCity] = useState('');
-  const [selectedDistrict, setSelectedDistrict] = useState('');
 
   const fetchgetMember = (uuid) => {
     const url = `http://localhost:3005/api/member/${uuid}`;
@@ -34,7 +30,7 @@ export default function PeopleInfo() {
       .then(response => {
         if (!response.ok) {
           return response.json().then(errorData => {
-            throw new Error(`错误 ${response.status}: ${errorData.message}`);
+            throw new Error(`錯誤 ${response.status}: ${errorData.message}`);
           });
         }
         return response.json();
@@ -42,12 +38,28 @@ export default function PeopleInfo() {
       .then(data => {
         if (data.status === 'success') {
           setUser(data.result);
+          if (data.result.avatar_file) {
+            // 檢查頭像是否存在
+            fetch(`/upload/${data.result.avatar_file}`)
+              .then(response => {
+                if (response.ok) {
+                  setAvatar(`/upload/${data.result.avatar_file}`);
+                } else {
+                  // 如果頭像不存在，使用預設頭像
+                  setAvatar(null);
+                }
+              })
+              .catch(() => setAvatar(null));
+          } else {
+            setAvatar(null);
+          }
         } else {
           setError(data.message);
         }
       })
       .catch(error => {
         setError(error.message);
+        setAvatar(avatarPic); // 發生錯誤時也設置為預設頭像
       })
       .finally(() => {
         setLoading(false);
@@ -65,12 +77,43 @@ export default function PeopleInfo() {
     }
   }, [authUser, authLoading]);
 
-  // 處理 TWZipCode 選擇變更的函數
-  const handleAddressChange = (city, district) => {
-    console.log('City changed to:', city);
-    console.log('District changed to:', district);
-    setSelectedCity(city);
-    setSelectedDistrict(district);
+  // 頭像點擊
+  const handleAvatarClick = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setAvatar(reader.result);
+      };
+      reader.readAsDataURL(file);
+      setUser({ ...user, avatar_file: file });
+    }
+    const formData = new FormData();
+    formData.append('avatar', file);
+
+    fetch(`http://localhost:3005/api/member/uploadAvatar/${user.id}`, {
+      method: 'POST',
+      body: formData,
+    })
+      .then(response => response.json())
+      .then(data => {
+        if (data.status === 'success') {
+          const filePath = `/upload/${authUser.uuid}.png`;
+          setAvatar(filePath); // 更新頭像路徑
+          setUser({ ...user, avatar_file: filePath });
+        } else {
+          setError(data.message);
+        }
+      })
+      .catch(error => {
+        setError(error.message);
+      });
   };
 
   // 提交表單處理函數
@@ -78,13 +121,13 @@ export default function PeopleInfo() {
     event.preventDefault();
 
     const formData = new FormData();
-    formData.append('name', user.name);
-    formData.append('gender', user.gender);
-    formData.append('dob', user.dob);
-    formData.append('tel', user.tel);
-    formData.append('address', `${selectedCity}${selectedDistrict}${user.address}`);
+    formData.append('nickname', user.nickname);
+    formData.append('introduce', user.introduce);
+    if (user.avatar_file) {
+      formData.append('avatar_file', user.avatar_file);
+    }
 
-    const url = `http://localhost:3005/api/member/${authUser.uuid}`;
+    const url = `http://localhost:3005/api/member/ForumMemberInfo/${authUser.uuid}`;
 
     fetch(url, {
       method: 'PUT',
@@ -102,55 +145,70 @@ export default function PeopleInfo() {
         setError(error.message);
       });
   };
+
+  const formatDate = (dateString) => {
+    if (!dateString) return '未知';
+
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+
+    return `${year}-${month}-${day}`;
+  };
+
   return (
     <>
       <main className={scss.ForumMemberInfo}>
         <div className="col-1 col-lg-4"></div>
         <div className={`${scss.midarea} col-12 col-lg-5`}>
-          <div className={scss.midtext}>
-            <div className={scss.area1}>頭像上傳 <div className={scss.avatarPic}><Image className="imgWrap" src={avatarPic} alt="Image" /></div></div>
-            <div className={scss.area2}>暱稱 <input placeholder="討論區暱稱"></input></div>
+          <form className={`${scss.midtext}`} onSubmit={handleSubmit}>
+            <div className={scss.area1}>頭像上傳
+              <div className={scss.avatarPic} onClick={handleAvatarClick}>
+                <div className={scss.avatarWrapper}>
+                  <Image
+                    className="imgWrap"
+                    src={avatar || avatarPic} // 使用 || 運算符確保總是有一個有效的值
+                    alt="Image"
+                    layout="fill"
+                    objectFit="cover"
+                  />
+                </div>
+              </div>
+              <input
+                type="file"
+                ref={fileInputRef}
+                style={{ display: 'none' }}
+                onChange={handleFileChange}
+              /></div>
+            <div className={scss.area2}>暱稱
+              <input value={user.nickname} onChange={(e) => setUser({ ...user, nickname: e.target.value })}
+                placeholder="討論區暱稱">
+              </input>
+            </div>
             <div className={scss.area3}>性別
               <div className={scss.genderRadio}>
-                <input type="radio"
-                  name="gender"
-                  value="1"
-                  id="male"
-                  checked={user.gender === '1'}
-                  onChange={() => setUser({ ...user, gender: '1' })}>
-                </input>
-                <label htmlFor="male">先生</label>
-                <input
-                  type="radio"
-                  name="gender"
-                  value="2"
-                  id="female"
-                  checked={user.gender === '2'}
-                  onChange={() => setUser({ ...user, gender: '2' })}
-                />
-                <label htmlFor="female">女士</label>
+                <p>{user.gender === 1 ? '先生' : '女士' || '-'}</p>
               </div>
             </div>
             <div className={scss.area4}>生日
-              <div><label html="birthday"></label>
-                <input type="month" id="birthday" name="birthday" min="1900-01" max="2023-12" required></input>
-              </div>
+              <p>{formatDate(user.dob) || '-'}</p>
             </div>
             <div className={scss.area5}>
               所在地
               <div className={`${scss.area5 - 2} px-2`}>
-                <TWZipCode onSelectionChange={handleAddressChange} />
+                <p>{user.address || '-'}</p>
               </div>
             </div>
             <div className={scss.area6}>有效文章 <p>3篇</p></div>
             <div className={scss.area7}>自我介紹
-              <textarea placeholder="如活潑、安靜、友善等..."></textarea>
+              <textarea value={user.introduce} onChange={(e) => setUser({ ...user, introduce: e.target.value })} placeholder="如活潑、安靜、友善等..."></textarea>
             </div>
             <div className={`${scss.botarea} my-5 mx-5`}>
               <button className={scss.btn1}>編輯資料</button>
               <button className={scss.btn2}>確認送出</button>
             </div>
-          </div>
+          </form>
         </div>
         <div className="col-3 my-5 d-none d-lg-block">
           <SideText></SideText>
