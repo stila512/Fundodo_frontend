38dbe18b6bdcce70f2f49879f8c01806a56dafd8
WarmWcHS <ironknees0712@gmail.com>
2024-08-26 14:07:45 +0800

    發文id

--- pages/article/contentItems/articleContent.js
+++ pages/article/contentItems/articleContent.js
@@ -44,7 +44,7 @@ export default function ArticleContent() {
               height={40}
             />
             <div className={[scss.nicknameArea].join()}>
-              <p className={[scss.nickName].join()}>123123123</p>
+              <p className={[scss.nickName].join()}>{content.author_nickname || '未知用戶'}</p>
               <p className={[scss.creatTime].join()}>{formatDate(content.create_at)}</p>
             </div>
           </div>
