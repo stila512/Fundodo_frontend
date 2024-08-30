8af3409282cc4a130b0b992c25ee00e5f7994a2e
Zooey <stila512@gmail.com>
2024-08-30 16:45:11 +0800

    修正課程觀看權限UI轉換/課程列表內容/加入購物車

--- pages/course/courseGrid.js
+++ pages/course/courseGrid.js
@@ -12,10 +12,10 @@ export default function CourseGrid({ courses }) {
             <div className={scss.cardWrapper}>
                 {courses.map((v) => {
                     return (
-                        <div className='col-6 col-lg-4'>
+                        <div className={['col-6 col-lg-4', scss.cards].join(" ")}>
                             <div key={v.id} className={scss.card}>
-                                <Link href={`/course/detail/${v.id}`} >
-                                    <div className={scss.crsPic}>
+                                <div className={scss.crsPic}>
+                                    <Link href={`/course/detail/${v.id}`} >
                                         <Image
                                             src={`http://localhost:3005/upload/crs_images/${v.img_path}`}
                                             alt={v.title}
@@ -23,17 +23,22 @@ export default function CourseGrid({ courses }) {
                                             objectFit="cover"
                                             className={scss.courseImage}
                                         />
+                                    </Link>
+                                </div>
+                                <div className={scss.cardBody}>
+                                    <h3>{v.title}</h3>
+                                    <p className={scss.summary}>{v.summary}</p>
+                                    <div className='d-flex gap-1 ai-center'>
+                                        <AiOutlineUser /> <p>{v.viewed_count} 人來學習</p>
                                     </div>
-                                    <div className={scss.cardBody}>
-                                        <h3>{v.title}</h3>
-                                        <div className='d-flex gap-1 ai-center'>
-                                            <AiOutlineUser /> <p>{v.viewed_count} 人來學習</p>
+                                </div>
+                                <div className="d-flex jc-center my-3">
+                                            <Link href={`/course/detail/${v.id}`}>
+                                                <button className={scss.button}>立即購買</button>
+                                            </Link>
                                         </div>
-                                    </div>
-                                </Link>
                             </div>
                         </div>
-
                     )
                 })}
             </div>
