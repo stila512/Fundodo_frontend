4a7fc492287d119548403e4e928fca134f5249a0
RediChen <a0929052958@gmail.com>
2024-09-02 23:23:46 +0800

    更新購物車 layout - top header 同步 default 的更新 - 修正 progress 的 RWD

--- components/layout/default/navHeader.js
+++ components/layout/default/navHeader.js
@@ -4,13 +4,12 @@ import { breakpoints } from '@/configs';
 import { useState } from 'react';
 //== Components ================================================================
 import Logo from '@/components/common/logo';
-import FddBtn from '@/components/buttons/fddBtn';
 import NavFuncBtns from '../NavFuncBtns';
 import NavLinks from '../NavLinks';
 //== Styles =================================================================
-import { GiHamburgerMenu } from "react-icons/gi";
 import useScreenWidth from '@/hooks/useScreenWidth';
 import scss from '../navHeader.module.scss';
+import NavToggleBtn from '../NavToggleBtn';
 
 /**
  * 導覽列 | Fundodo 頁面基本架構
@@ -25,16 +24,7 @@ export default function NavHeader() {
       <div className='container h-100'>
         <div className={scss.headerMembrane}>
           <div className='d-block d-md-none flex-grow-1 flex-lg-grow-0'>
-            <FddBtn
-              color='white'
-              pill={false}
-              icon
-              className={[scss.hbgBtn, showMenu ? scss.active : ''].join(' ')}
-              callback={() => setShowMenu(!showMenu)}
-            >
-              {/* <GiHamburgerMenu /> */}
-              <div className={scss.bar}></div>
-            </FddBtn>
+            <NavToggleBtn showMenu={showMenu} setShowMenu={setShowMenu} />
           </div>
           <div className='pos-a t-50 l-50 translate-middle pos-md-r t-md-0 l-md-0 translate-middle-md-none'>
             <Logo width={210} href="/"></Logo>
