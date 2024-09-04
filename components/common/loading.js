73d15de6471a01ba0e6afc516a98dfc71ea37d66
RediChen <a0929052958@gmail.com>
2024-09-04 17:32:10 +0800

    [x]初始載入動畫失敗，暫時存檔

--- components/common/loading.js
+++ components/common/loading.js
@@ -3,7 +3,13 @@ import React from 'react'
 
 export default function Loading() {
   return (
-    <div className='min-vh-100 gr-center' style={{ backgroundColor: "#F4D284" }}>
+    <div
+      className='min-vh-100 gr-center pos-a t-0 l-0 r-0'
+      style={{
+        backgroundColor: "#F4D284",
+        zIndex: 999
+      }}
+    >
       <Image src='/loading.gif' width={150} height={150} alt='loading fundodo logo' />
     </div>
   )
--- components/layout/default/index.js
+++ components/layout/default/index.js
@@ -1,17 +1,14 @@
-// import { Suspense } from 'react';
+import { Suspense } from 'react';
 import NavHeader from './navHeader';
 import Footer from './footer';
-// import Loading from '@/components/common/loading.js';
-// import dynamic from 'next/dynamic';
+import Loading from '@/components/common/loading.js';
+import GoTop from '@/components/common/goTop';
 
 /**
  * Fundodo 頁面基本架構 | 預設版本
  * @description 請以此元件包住頁面內容
  */
 export default function DefaultLayout({ children }) {
-  // const Loader = dynamic(() => import('@/components/common/loading'));
-  // const Trending = dynamic(() => import('./Trending'), {
-  //   ssr: false, loading: () => <Loader />,
   // })
   return (
     <>
@@ -19,6 +16,7 @@ export default function DefaultLayout({ children }) {
       {/* <Suspense fallback={<Loading />}> */}
       <div style={{ marginTop: '80px' }}>{children}</div>
       {/* </Suspense> */}
+      <GoTop />
       <Footer />
     </>
   );
