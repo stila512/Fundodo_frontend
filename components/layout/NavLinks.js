d159dc8bde8f7d481d162f114e602d308021e0b9
RediChen <a0929052958@gmail.com>
2024-09-08 20:58:56 +0800

    [fix] 修正導覽列 RWD - 新增手機板的登入登出鈕 - 修正對電腦版的影響

--- components/layout/NavLinks.js
+++ components/layout/NavLinks.js
@@ -1,14 +1,34 @@
 import Link from 'next/link';
 import scss from './navLink.module.scss';
-import { useContext } from 'react';
+import { useRouter } from 'next/router';
+import { useState, useEffect, useContext } from 'react';
 import { AuthContext } from '@/context/AuthContext';
 
 export default function NavLinks({ toggleLinks = () => { } }) {
+  const router = useRouter();
 
-  const { user } = useContext(AuthContext);
+  const { user, logout } = useContext(AuthContext);
+  //===== 會員 ID
+  //0 | 未登入 ; -1 | 讀取中
+  /** @type {[number, React.Dispatch<number>]} */
+  const [uID, setUID] = useState(-1);
+
+  useEffect(() => {
+    (user === null) ? setUID(0) : setUID(user.userId);
+  }, [user])
 
   //判斷管理員登入
   const isAdmin = user && user.user_level >= 20;
+
+  const handleLog = () => {
+    if (uID <= 0) {
+      router.push('/member/login');
+    } else {
+      setUID(-1);
+      logout();
+    }
+    toggleLinks(false);
+  }
 
   return (
     <ul className={scss.ulLink}>
@@ -32,6 +52,18 @@ export default function NavLinks({ toggleLinks = () => { } }) {
           <Link href="/backend/course">後台管理</Link>
         </li>
       )}
+      <li className={scss.listBtn} onClick={() => toggleLinks(false)}>
+        <button
+          onClick={() => handleLog()}
+          style={{
+            backgroundColor: 'transparent',
+            border: 'none'
+          }}
+          type='button'
+        >
+          {uID <= 0 ? '登入 / 註冊' : '登出'}
+        </button>
+      </li>
     </ul>
   );
 }
